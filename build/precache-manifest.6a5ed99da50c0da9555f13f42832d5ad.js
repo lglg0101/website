f6f90e2e4022c4f2d49b3619553984f42c4d7c3d
your username <lissygundersen@gmail.com>
2020-03-11 17:37:17 +0000

    deploying

---
+++ build/precache-manifest.6a5ed99da50c0da9555f13f42832d5ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ab316e3063abfc2150b502042898cdb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2887d7686c97ec28ceaf",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f82250c61c50a2e03c65",
+    "url": "/static/js/2.1c78273f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.1c78273f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2887d7686c97ec28ceaf",
+    "url": "/static/js/main.f2ba9191.chunk.js"
+  },
+  {
+    "revision": "e45e20948a247110e75b",
+    "url": "/static/js/runtime-main.46b10d78.js"
+  }
+]);
